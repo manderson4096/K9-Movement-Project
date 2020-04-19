c96c19e414aab1f700bedc45459d152b3b5fb987
Julia <WorrellJ1725@uhcl.edu>
2020-04-19 13:17:28 -0500

    Integrating image analysis code with webcamera code

--- k9 windows-2/k9 windows/Form1.Designer.cs
+++ k9 windows-2/k9 windows/Form1.Designer.cs
@@ -28,33 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.picOutput = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.picStream = new System.Windows.Forms.PictureBox();
             this.btnStreamOnOff = new System.Windows.Forms.Button();
             this.SveBtn = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.picOutput)).BeginInit();
+            this.btnAnalyze = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.picStream)).BeginInit();
             this.SuspendLayout();
-            // 
-            // picOutput
-            // 
-            this.picOutput.Location = new System.Drawing.Point(582, 34);
-            this.picOutput.Name = "picOutput";
-            this.picOutput.Size = new System.Drawing.Size(313, 252);
-            this.picOutput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.picOutput.TabIndex = 0;
-            this.picOutput.TabStop = false;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(161, 382);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 34);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Capture";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // picStream
             // 
@@ -67,7 +46,7 @@
             // 
             // btnStreamOnOff
             // 
-            this.btnStreamOnOff.Location = new System.Drawing.Point(126, 326);
+            this.btnStreamOnOff.Location = new System.Drawing.Point(56, 317);
             this.btnStreamOnOff.Name = "btnStreamOnOff";
             this.btnStreamOnOff.Size = new System.Drawing.Size(148, 34);
             this.btnStreamOnOff.TabIndex = 3;
@@ -77,7 +56,7 @@
             // 
             // SveBtn
             // 
-            this.SveBtn.Location = new System.Drawing.Point(719, 316);
+            this.SveBtn.Location = new System.Drawing.Point(280, 388);
             this.SveBtn.Name = "SveBtn";
             this.SveBtn.Size = new System.Drawing.Size(75, 23);
             this.SveBtn.TabIndex = 5;
@@ -85,32 +64,38 @@
             this.SveBtn.UseVisualStyleBackColor = true;
             this.SveBtn.Click += new System.EventHandler(this.SveBtn_Click);
             // 
+            // btnAnalyze
+            // 
+            this.btnAnalyze.Location = new System.Drawing.Point(56, 383);
+            this.btnAnalyze.Name = "btnAnalyze";
+            this.btnAnalyze.Size = new System.Drawing.Size(176, 33);
+            this.btnAnalyze.TabIndex = 7;
+            this.btnAnalyze.Text = "Analyze Image";
+            this.btnAnalyze.UseVisualStyleBackColor = true;
+            this.btnAnalyze.Click += new System.EventHandler(this.btnAnalyze_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1023, 466);
+            this.ClientSize = new System.Drawing.Size(543, 447);
+            this.Controls.Add(this.btnAnalyze);
             this.Controls.Add(this.SveBtn);
             this.Controls.Add(this.btnStreamOnOff);
             this.Controls.Add(this.picStream);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.picOutput);
             this.Name = "Form1";
             this.Text = "WebCam Output";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.picOutput)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picStream)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox picOutput;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox picStream;
         private System.Windows.Forms.Button btnStreamOnOff;
         private System.Windows.Forms.Button SveBtn;
+        private System.Windows.Forms.Button btnAnalyze;
     }
 }
 
--- k9 windows-2/k9 windows/Form1.cs
+++ k9 windows-2/k9 windows/Form1.cs
@@ -1,5 +1,7 @@
 ﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
+using Emgu.CV.Util;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -15,7 +17,8 @@ namespace k9_windows
     public partial class Form1 : Form
     {
         bool _streaming;
-        Capture _capture;
+        Capture _capture; //defining capture object, will instansiate withing event handler
+        Capture _analyzecapture;//defining capture object, will instansiate withing event handler
         public Form1()
         {
             InitializeComponent();
@@ -24,6 +27,7 @@ namespace k9_windows
         private void Form1_Load(object sender, EventArgs e)
         {
             _capture = new Capture();
+            _analyzecapture = new Capture();
             _streaming = false;
 
         }
@@ -49,19 +53,14 @@ namespace k9_windows
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)//Event handler for capture button
-        {
 
-            picOutput.Image = picStream.Image;
-
-        }
 
         private void SveBtn_Click(object sender, EventArgs e)//Event handler to Save image that was captured
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             if(saveFileDialog.ShowDialog() == DialogResult.OK)
             {
-                picOutput.Image.Save(saveFileDialog.FileName+ ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);//saves captured image to any file format you want
+                picStream.Image.Save(saveFileDialog.FileName+ ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);//saves captured image to any file format you want
                 MessageBox.Show(@"Saved!");
             }
 
@@ -79,6 +78,173 @@ namespace k9_windows
 
         }
 
+        //event handler for hitting the analyze button
+        private void btnAnalyze_Click(object sender, EventArgs e)
+        {
+            var img = _analyzecapture.QueryFrame().ToImage<Bgr, byte>();
 
+            // read in input image
+            // Console.WriteLine("Enter a file to analyze: ");
+            //string imageInput = Console.ReadLine();
+            //Image<Bgr, Byte> img = new Image<Bgr, byte>(imageInput);
+
+            // convert image to grayscale
+            UMat uimage = new UMat();
+            CvInvoke.CvtColor(img, uimage, ColorConversion.Bgr2Gray);
+
+            // remove noise from image
+            UMat pyrDown = new UMat();
+            CvInvoke.PyrDown(uimage, pyrDown);
+            CvInvoke.PyrUp(pyrDown, uimage);
+
+            // find the circles
+            double cannyThreshold = 180.0;
+            double circleAccumulatorThreshold = 120;
+            CircleF[] circles = CvInvoke.HoughCircles(uimage, HoughType.Gradient, 2.0, 20.0, cannyThreshold, circleAccumulatorThreshold, 5);
+
+
+            // find edges
+            double cannyThresholdLinking = 120.0;
+            UMat cannyEdges = new UMat();
+            CvInvoke.Canny(uimage, cannyEdges, cannyThreshold, cannyThresholdLinking);
+
+            LineSegment2D[] lines = CvInvoke.HoughLinesP(
+               cannyEdges,
+               1,               //Distance resolution in pixel-related units
+               Math.PI / 45.0,  //Angle resolution measured in radians.
+               20,              //threshold
+               30,              //min Line width
+               10);             //gap between lines
+
+
+            // find rectangles
+            List<RotatedRect> boxList = new List<RotatedRect>();
+
+            using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
+            {
+                CvInvoke.FindContours(cannyEdges, contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+                int count = contours.Size;
+                for (int i = 0; i < count; i++)
+                {
+                    using (VectorOfPoint contour = contours[i])
+                    using (VectorOfPoint approxContour = new VectorOfPoint())
+                    {
+                        CvInvoke.ApproxPolyDP(contour, approxContour, CvInvoke.ArcLength(contour, true) * 0.05, true);
+                        if (CvInvoke.ContourArea(approxContour, false) > 250) //only consider contours with area greater than 250
+                        {
+                            if (approxContour.Size == 4) // rectangles have 4 contours (vertices)
+                            {
+                                bool isRectangle = true;
+                                Point[] pts = approxContour.ToArray();
+                                LineSegment2D[] edges = PointCollection.PolyLine(pts, true);
+
+                                for (int j = 0; j < edges.Length; j++)
+                                {
+                                    double angle = Math.Abs(
+                                       edges[(j + 1) % edges.Length].GetExteriorAngleDegree(edges[j]));
+                                    if (angle < 80 || angle > 100)
+                                    {
+                                        isRectangle = false;
+                                        break;
+                                    }
+                                }
+
+                                if (isRectangle) boxList.Add(CvInvoke.MinAreaRect(approxContour));
+                            }
+                        }
+                    }
+                }
+            }
+
+            // print detected rectangles to output file
+            Image<Bgr, Byte> rectangleImage = img.CopyBlank();
+            foreach (RotatedRect box in boxList)
+                rectangleImage.Draw(box, new Bgr(Color.DarkOrange), 2);
+            rectangleImage.Save("output-rectangles.bmp");
+
+            // print detected circles to output file
+            Image<Bgr, Byte> circleImage = img.CopyBlank();
+            foreach (CircleF circle in circles)
+                circleImage.Draw(circle, new Bgr(Color.Green), 2);
+            circleImage.Save("output-circles.bmp");
+
+
+            // function to detect if target center X values match
+            bool detectTargetX(CircleF circle, RotatedRect rectangle) =>
+                (circle.Center.X > (rectangle.Center.X) * 0.99) &&
+                (circle.Center.X < (rectangle.Center.X) * 1.01) ? true : false;
+
+            // function to detect if target center Y values match
+            bool detectTargetY(CircleF circle, RotatedRect rectangle) =>
+                (circle.Center.Y > (rectangle.Center.Y) * 0.99) &&
+                (circle.Center.Y < (rectangle.Center.Y) * 1.01) ? true : false;
+
+            // checking for target in image
+            bool targetFound = false;
+            CircleF targetCircle = new CircleF();
+            foreach (CircleF circle in circles)
+            {
+                foreach (RotatedRect box in boxList)
+                {
+                    if (detectTargetX(circle, box) == true) // check if X values match
+                    {
+                        if (detectTargetY(circle, box) == true) // check if Y values match
+                        {
+                            //Console.WriteLine("Target is found at " + circle.Center);
+                            MessageBox.Show("Target is found at " + circle.Center);
+                            targetCircle = circle;
+                            targetFound = true;
+                            break;
+                        }
+                        else
+                            continue;
+
+                    }
+
+                    else if (targetFound == true) // if target is found, stop the loop
+                        break;
+
+                    else
+                        continue;
+                }
+            }
+
+
+            // find image zones used for movement determinations
+            double FindLeftBound(UMat image) => image.Size.Width * 0.45;
+            double FindRightBound(UMat image) => image.Size.Width * 0.55;
+            double FindUpperBound(UMat image) => image.Size.Height * 0.40;
+            double FindLowerBound(UMat image) => image.Size.Height * 0.60;
+
+            // determine if K9 needs to move left or right
+            string DetermineHorizontal(CircleF target, UMat image) =>
+                target.Center.X < FindLeftBound(image) ?
+                "Move right to center the target" : target.Center.X > FindRightBound(image) ?
+                "Move left to center the target" : "Target is horizontally centered";
+
+            // determine if K9 needs to tilt head up or down
+            string DetermineVertical(CircleF target, UMat image) =>
+                target.Center.Y > FindLowerBound(image) ?
+                "Tilt head up to center the target" : target.Center.Y < FindUpperBound(image) ?
+                "Tilt head down to center the target" : "Target is vertically centered";
+
+            // find image area limit that the target should fill
+            double FindAreaLimit(UMat image) => image.Size.Height * image.Size.Width * 0.10;
+
+            // determine if K9 needs to move forward
+            string DetermineMovement(CircleF target, UMat image) =>
+                target.Area > FindAreaLimit(image) ?
+                "Stop (no forward movement" : "Move forward";
+
+            if (targetFound == true)
+            {
+                
+                MessageBox.Show(DetermineHorizontal(targetCircle, uimage) +". \n\n"+ DetermineVertical(targetCircle, uimage)+ ". \n\n" + DetermineMovement(targetCircle, uimage) + ".");
+                
+            }
+            else
+               //Console.WriteLine("Target was not found.");
+                MessageBox.Show("Target was not found.");
+        }
     }
 }
